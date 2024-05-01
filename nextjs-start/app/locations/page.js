a9b1534efdcc7c4ac23bc830971ef6cd3c23b4c9
Kristoffer Kristensen <62994390+PK-Kristensen@users.noreply.github.com>
2024-05-01 09:27:58 +0200

    Update page.js

--- nextjs-start/app/locations/page.js
+++ nextjs-start/app/locations/page.js
@@ -1,5 +1,5 @@
-import FetchLocations from "../../components/locations/LocationListing.jsx";
-import ImportCsvToFirebase from "../../components/locations/ImportCSV.jsx";
+import FetchLocations from "../../src/components/locations/LocationListing";
+//import ImportCsvToFirebase from "../components/locations/ImportCSV.jsx";
 export default function Home() {
   return (
     <main className="p-8 m-4 bg-white shadow rounded-lg">
